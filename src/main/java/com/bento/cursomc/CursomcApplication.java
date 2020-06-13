9678969e9871a9a8bd2a095278e43876c12eca23
luis bento <lfbento@gmail.com>
2020-06-13 08:53:07 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/bento/cursomc/CursomcApplication.java
+++ src/main/java/com/bento/cursomc/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.bento.cursomc;
 
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +9,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.bento.cursomc.domain.Categoria;
-import com.bento.cursomc.domain.Produto;
-import com.bento.cursomc.domain.Estado;
 import com.bento.cursomc.domain.Cidade;
+import com.bento.cursomc.domain.Cliente;
+import com.bento.cursomc.domain.Endereco;
+import com.bento.cursomc.domain.Estado;
+import com.bento.cursomc.domain.Produto;
+import com.bento.cursomc.domain.enums.TipoCliente;
 import com.bento.cursomc.repositories.CategoriaRepository;
 import com.bento.cursomc.repositories.CidadeRepository;
+import com.bento.cursomc.repositories.ClienteRepository;
+import com.bento.cursomc.repositories.EnderecoRepository;
 import com.bento.cursomc.repositories.EstadoRepository;
 import com.bento.cursomc.repositories.ProdutoRepository;
 
@@ -27,6 +33,11 @@ public class CursomcApplication implements CommandLineRunner {
 	private EstadoRepository estadoRepository;
 	@Autowired
 	private CidadeRepository cidadeRepository;
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+	
 	
 	
 	public static void main(String[] args) {
@@ -66,6 +77,19 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente cli1 = new Cliente(null,"Maria Silva", "maria@gmail.com", "363378912377", TipoCliente.PESSOAFISICA);
+		
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+		
+		Endereco e1 = new Endereco(null, "Rua Flores", "300", "Apto 303", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+		
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+		
 		
 		
 		
---
+++ src/main/java/com/bento/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,14 @@
+package com.bento.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bento.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+	
+	
+	
+
+}
